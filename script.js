e769c9663d874d4ddef40492da213c765edbf4b3
MaskedByte660 <codingtechinfo01@gmail.com>
2025-02-11 10:38:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make = make;
+	this.model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+	Car.call(this, make, model);
+	this.topSpeed = topSpeed;
+}
+
+//create class car-> add properties to it and its method
+
+//create sportsCar class ->  inherit Car -> create properties and method
+// class Car{
+// 	constructor(make, model){
+// 		this.make = make;
+// 		this.model = model;
+// 	}
+// }
+
+Car.prototype.getMakeModel = function(){
+	return `${this.make} ${this.model}`;
+}
+
+// class SportsCar extends Car{
+// 	constructor(make, model, topSpeed){
+// 		super(make, model);
+// 		this.topSpeed = topSpeed;
+// 	}
+// }
+
+SportsCar.prototype.getTopSpeed = function(){
+	return this.topSpeed;
+}
 
 // Do not change the code below
 window.Car = Car;
